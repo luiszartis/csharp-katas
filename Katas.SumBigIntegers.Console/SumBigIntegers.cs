df6cebe80ec491306efc045f58a3e535e5362c91
Luis Barrientos Margolles <hector.barrientos@zartis.com>
2023-09-07 09:40:17 +0200

    Add SumBigintegers kata

---
+++ Katas.SumBigIntegers.Console/SumBigIntegers.cs
@@ -0,0 +1,15 @@
+﻿using System.Numerics;
+
+namespace Katas.SumBigIntegers.Console;
+
+public class SumBigIntegers
+{
+    public BigInteger Sum(List<BigInteger> list, int size)
+    {
+        if (list.Count != size)
+        {
+            throw new Exception("Stack over flow");
+        }
+        return list.Aggregate(BigInteger.Add);
+    }
+}
---
+++ Katas.SumBigintegers.Tests/SumBigIntegersTests.cs
@@ -0,0 +1,45 @@
+using System.Numerics;
+using AutoFixture;
+using FluentAssertions;
+using Xunit;
+
+namespace Katas.SumBigIntegers.Tests;
+
+public class SumIntegerTests
+{
+    private readonly Console.SumBigIntegers _sut;
+
+    public SumIntegerTests()
+    {
+        IFixture fixture = new Fixture();
+        _sut = fixture.Create<Console.SumBigIntegers>();
+    }
+
+    [Fact]
+    public void Sum_WhenSizeDifferentFromTheList_RaisesException()
+    {
+        // arrange
+        var list = new List<BigInteger>() { 2, 4, 4 };
+
+        // act
+        var action = () =>  _sut.Sum(list, list.Count + 1);
+
+        // assert
+        var ex = action.Should().Throw<Exception>();
+        ex.WithMessage("Stack over flow");
+    }
+
+    [Fact]
+    public void Sum_WhenSizeIsSameAsList_ReturnSum()
+    {
+        // arrange
+        var list = new List<BigInteger>() { 1000000001, 1000000002, 1000000003, 1000000004, 1000000005 };
+        
+        // act
+        var result = _sut.Sum(list, list.Count);
+        
+        // assert
+        result.Should().Be(list.Aggregate(BigInteger.Add));
+        result.Should().Be(5000000015);
+    }
+}
